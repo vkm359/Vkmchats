9f3b68d455794a88cf602881c5af46493ba54c2e
vkm359 <vkseriya06@gmail.com>
2026-09-05 20:54:03 +0530

    Refactor server.js to use Express framework

--- server.js
+++ server.js
@@ -92,4 +92,15 @@ app.use(express.static(__dirname));
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-    
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
