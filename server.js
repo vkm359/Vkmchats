8ea52814e8e9718ed9a03ff241098e10204cb520
vkm359 <vkseriya06@gmail.com>
2026-09-05 20:38:19 +0530

    Refactor server to use Express for static files

--- server.js
+++ server.js
@@ -82,3 +82,14 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`VKM Chats Server running on port ${PORT}`));
 
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000:
+app.use(express.static(__dirname));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+    
